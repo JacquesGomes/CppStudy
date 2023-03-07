4b652d504b4d78be405d929f4d2c58e9395973c4
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-03-07 10:28:43 -0300

    Basic

---
+++ Bash/compile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int x;
+    cin >> x;
+    if(x == 0){
+        cout << "Zero my friends!" << endl;
+    }
+    else{
+        cout << "Not zero my friends!" << endl;
+    }
+}
---
+++ Bash/compile.sol
@@ -0,0 +1,1 @@
+Not zero my friends!
